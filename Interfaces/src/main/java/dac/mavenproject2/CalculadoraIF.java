e7d37e92ad38c4ea33c8a303e0bfde1782b7f2f4
Antonio Junior <Antonio Junior@Junior-PC>
2015-12-03 10:10:41 -0300

    copilado

---
+++ Interfaces/src/main/java/dac/mavenproject2/CalculadoraIF.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dac.mavenproject2;
+
+/**
+ *
+ * @author Anderson Souza
+ */
+public interface CalculadoraIF {
+    
+    public double multiplicar(int n1, int n2);
+    
+    public double dividir(int n1, int n2);
+    
+    
+    
+}
